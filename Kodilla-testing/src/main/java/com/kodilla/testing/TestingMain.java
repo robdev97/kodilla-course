a0fdf98144742e495224d9a5572d9b736908e884
Robert Litwiński <robert.lit97@gmail.com>
2024-11-13 23:45:22 +0100

    Pierwszy test jednostkowy

--- Kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ Kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testów oprogramowania");
 
+        Calculator calculator = new Calculator();
+
+        int result1 = calculator.add(10, 5);
+        System.out.println(result1);
+
+        if(result1 == 15) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Test FAILED");
+        }
+
+        int result2 = calculator.subtract(10, 5);
+        System.out.println(result2);
+
+        if(result2 == 5) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Test FAILED");
+        }
     }
 }
