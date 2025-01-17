d8b4c533f36643f8b50a3c7c48efcd21bf4295a2
Robert Litwiński <robert.lit97@gmail.com>
2025-01-17 03:04:19 +0100

    16.2 Zadanie: Tworzymy encję list zadań.

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,71 @@
+package com.kodilla.hibernate.tasklist;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "TASKLIST")
+public final class TaskList {
+
+
+    private int id;
+    private String listName;
+    private String description;
+
+    public TaskList() {
+    }
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    @NotNull
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskList taskList = (TaskList) o;
+        return Objects.equals(id, taskList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "Test: findByNameList";
+    private static final String LISTNAME = "Test:ListName";
+
+    @Test
+    void testFindByListName() {
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+        taskListDao.save(taskList);
+
+        // When
+        List<TaskList> findByListName = taskListDao.findByListName(LISTNAME);
+
+        // Then
+        assertEquals(13, findByListName.size());
+        assertEquals(LISTNAME, findByListName.get(0).getListName());
+
+        // CleanUp
+        taskListDao.delete(taskList);
+    }
+}
